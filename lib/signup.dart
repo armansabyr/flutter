7c2c194384487eb1573d92321fa5b1f4d977fff3
armansabyr <armansabyrov@gmail.com>
2018-07-04 14:59:21 +0600

    firts

---
+++ lib/signup.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class SignupPage extends StatefulWidget {
+
+  @override
+  _SignupPageState createState() => new _SignupPageState();
+}
+
+class _SignupPageState extends State<SignupPage> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+
+    );
+  }
+}
