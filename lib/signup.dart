f5e577c8588dae58d8ca2215004d6ddf00fcd4ba
armansabyr <armansabyrov@gmail.com>
2018-07-06 18:56:12 +0600

    suicude

--- lib/signup.dart
+++ lib/signup.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-
+import 'dart:io';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class SignupPage extends StatefulWidget {
   @override
@@ -17,7 +19,27 @@ class _SignupPageState extends State<SignupPage> {
 
   final formKey = new GlobalKey<FormState>();
 
+  void validateAndSave(){
+    final form = formKey.currentState;
+    if (form.validate()){
+      form.save();
+      print('First name $_firstName, email $_email');
 
+      var url = 'http://method-marketplace.herokuapp.com/signup';
+      var client = new http.Client();
+      var request = new http.Request('POST', Uri.parse(url));
+      var bytes = UTF8.encode(_password);
+      var base64 = BASE64.encode(bytes);
+      var body = {'email': _email, 'phone': _phone, 'first': _firstName,
+      'last': _lastName, 'enc_pass': base64};
+      request.headers[HttpHeaders.CONTENT_TYPE] = 'application/json; charset=utf-8';
+      request.body = json.encode(body);
+      var current = client.send(request).then( (response)
+        => response.stream.bytesToString().then((value)
+          =>print(value))).catchError((error)=> print(error.toString())) ;
+      print(value['ok'])
+    }
+  }
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -58,13 +80,12 @@ class _SignupPageState extends State<SignupPage> {
                   ),
                   new TextFormField(
                       decoration: new InputDecoration( labelText: "phone"),
-                      obscureText: true,
                       validator: (value) => value.isEmpty ? 'Price can\'t be empty':null,
                       onSaved: (value) => _phone = value //value as int,
                   ),
                   new RaisedButton(
                     child: new Text("Submit", style: new TextStyle(fontSize: 20.0)),
-                    onPressed: (){},
+                    onPressed: validateAndSave,
                   )
                 ],
               )
